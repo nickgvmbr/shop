ef5ce63a0be726ef212a40ce1d15760dc3d4a583
Nick <gvmbrtell@gmail.com>
2019-04-10 07:41:31 -0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <>App worky!</>
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import './style.sass'
+import App from './components/App'
+
+render(
+  <App />, document.querySelector('#root')
+)
